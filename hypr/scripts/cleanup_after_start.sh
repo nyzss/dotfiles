1e714015d4c0f3529e7a6ff7bb53b6d5711d2023
nyzss <neazenmeh@gmail.com>
2023-07-09 16:12:51 +0200

    script works better, mostly good

--- hypr/scripts/cleanup_after_start.sh
+++ hypr/scripts/cleanup_after_start.sh
@@ -1,6 +1,6 @@
 
 sleep 10
 hyprctl keyword windowrule "workspace unset,kitty"
-hyprctl keyword windowrule "workspace unset,google-chrome-stable"
+# hyprctl keyword windowrule "workspace unset,google-chrome-stable"
 # hyprctl keyword windowrule "workspace unset,discord"
 # hyprctl keyword windowrule "workspace unset,spotify"
